92c8a1576e450ba013b115a687dc793d1807cf13
Teodor Briceanu <teodorbriceanu@gamil.com>
2023-04-17 16:34:02 +0300

    creating form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,34 +1,37 @@
-import { mailOptions, transporter } from '@/lib/nodemailer';
+import nodemailer from 'nodemailer';
 
-const handler = async (req, res) => {
+const emailUser = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export default function sendData(req, res) {
   const { name, email, message } = req.body;
-  if (req.method === 'POST') {
-    const emailRegex = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/i;
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: 'Invalid email address' });
-    }
-    if (name.trim() === '') {
-      return res.status(400).json({ message: 'Name is required' });
-    }
-    try {
-      await transporter.sendMail({
-        ...mailOptions,
-        from: 'teodorbriceanu@gmail.com',
-        to: 'teodorbriceanu@gmail.com',
-        subject: 'this is a mail from your website',
-        html: `
-        <p>Sender: ${name}</p>
-        <p>${email}</p>
-        <p>${message}</p>
-        `,
-      });
-      return res.status(200).json({ sucess: true });
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json({ message: error.message });
-    }
-  }
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: emailUser,
+      pass,
+    },
+    secure: true,
+  });
 
-  return res.status(400).json({ message: 'Bad request' });
-};
-export default handler;
+  const mailData = {
+    from: 'teodorbriceanu@gmail.com',
+    to: 'teodorbriceanu@gmail.com',
+    subject: `Message From ${name}`,
+    html: `
+    <p>Sender: ${name}</p>
+    <p>${email}</p>
+    <p>${message}</p>
+    `,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Failed to send email');
+    } else {
+      console.log(info);
+      res.status(200).send('Message sent successfully');
+    }
+  });
+}
